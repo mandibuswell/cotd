a48435f50bb168155e9b4722e1c44141b70ebbef
eformat <eformat@gmail.com>
2016-11-22 16:36:41 +1000

    auckland

--- data/cats/rank.php
+++ data/cats/rank.php
@@ -10,10 +10,10 @@ $_SESSION['app'] = 'COTD';
 
 // Change ranking order here
 $_SESSION['ranks'] = array(
-    'auckland',    
     'adelaide',         
     'melbourne',
-    'canberra',    
+    'canberra',
+    'auckland',            
     'wellington',    
     'sydney',
     'brisbane',
